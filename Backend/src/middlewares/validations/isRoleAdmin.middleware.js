7850d37e93759b0e0ca11f150c3a2dc9a58d92a5
karvaroz <vargasriosk@gmail.com>
2023-06-04 14:54:11 -0500

    middleware user is role admin

---
+++ Backend/src/middlewares/validations/isRoleAdmin.middleware.js
@@ -0,0 +1,20 @@
+const { GenerateToken } = require("../../helpers");
+const { UserService } = require("../../services");
+
+const isRoleAdmin = async (req, res, next) => {
+	const token = req.headers.authorization.split(" ").pop();
+	const tokenData = await GenerateToken.verifyToken(token);
+
+	const userData = await UserService.getUserById(tokenData.id);
+
+	if (userData.role == "Admin") {
+		next();
+		return;
+	}
+
+	res.status(403).json({
+		status: "YOU DO NOT HAVE AN ADMIN ROLE",
+	});
+};
+
+module.exports = isRoleAdmin;
