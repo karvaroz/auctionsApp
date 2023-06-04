58f4033bf52578eecb2ed1a9a208c2f15417c4f2
karvaroz <vargasriosk@gmail.com>
2023-06-04 14:53:56 -0500

    mddleware user is authenticated

---
+++ Backend/src/middlewares/validations/isAuth.middleware.js
@@ -0,0 +1,16 @@
+const { GenerateToken } = require("../../helpers");
+
+const isAuthenticated = async (req, res, next) => {
+	const token = req.headers.authorization.split(" ").pop();
+	const tokenData = await GenerateToken.verifyToken(token);
+	if (tokenData) {
+		next();
+		return;
+	}
+
+	res.status(401).json({
+		status: "AUTHENTICATION FAILED",
+	});
+};
+
+module.exports = isAuthenticated;
