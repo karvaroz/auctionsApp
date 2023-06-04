8a506b56302d5aa5647c9976bb308edd446d78ec
karvaroz <vargasriosk@gmail.com>
2023-06-04 14:56:57 -0500

    Created user services

--- Backend/src/services/user.services.js
+++ Backend/src/services/user.services.js
@@ -1,45 +1,29 @@
-const { UserDB } = require("../database");
+const { HandleBcrypt } = require("../helpers");
+const { UserModel } = require("../models");
 
-const getAllUsers = async () => {
-	try {
-		return await UserDB.getAllUsers();
-	} catch (error) {
-		throw error;
-	}
+const getUserById = async (userId) => {
+    try {
+        return await UserModel.find({ _id: userId });
+    } catch (error) {
+        throw error;
+    }
 };
-const getOneUserById = async (userId) => {
-	try {
-		return await UserDB.getOneUserById(userId);
-	} catch (error) {
-		throw error;
-	}
-};
+
 const createNewUser = async (user) => {
-	try {
-		return await UserDB.createNewUser(user);
-	} catch (error) {
-		throw error;
-	}
-};
-const updateOneUserById = async (userId, dataUpdated) => {
-	try {
-		return await UserDB.updateOneUserById(userId, dataUpdated);
-	} catch (error) {
-		throw error;
-	}
-};
-const deleteOneUserById = async (userId) => {
-	try {
-		return await UserDB.deleteOneUserById(userId);
-	} catch (error) {
-		throw error;
-	}
+    try {
+        const { password } = user;
+        const passwordHash = await HandleBcrypt.encrypt(password);
+
+        const createUser = await new UserModel({
+            ...user,
+            password: passwordHash,
+        })
+
+        return await createUser.save();
+
+    } catch (error) {
+        throw error;
+    }
 };
 
-module.exports = {
-	getAllUsers,
-	getOneUserById,
-	createNewUser,
-	updateOneUserById,
-	deleteOneUserById,
-};
+module.exports = { getUserById, createNewUser };
