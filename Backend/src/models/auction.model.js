530f6ea21a1a8e467b6db8a989073babf2d13d1b
karvaroz <vargasriosk@gmail.com>
2023-06-04 11:46:49 -0500

    modified auction schema

--- Backend/src/models/auction.model.js
+++ Backend/src/models/auction.model.js
@@ -1,26 +1,38 @@
 const mongoose = require("mongoose");
 
-const Auction = mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
+const Auction = mongoose.Schema(
+	{
+		product: {
+			type: String,
+			required: true,
+		},
+		description: {
+			type: String,
+			required: false,
+			default: 0,
+		},
+		minBidAmount: {
+			type: Number,
+			required: true,
+			default: 100,
+		},
+		time: {
+			type: Number,
+			required: true,
+		},
+		image: {
+			type: String,
+			required: true,
+		},
+		status: {
+			type: String,
+			enum: ["Upcoming", "Open", "Closed"],
+			default: "Upcoming",
+		},
 	},
-	description: {
-		type: String,
-	},
-	quantity: {
-		type: Number,
-		required: false,
-		default: 0,
-	},
-	price: {
-		type: Number,
-		required: true,
-	},
-	category: {
-		type: String,
-		required: true,
-	},
-});
+	{
+		timestamps: true,
+	}
+);
 
 module.exports = mongoose.model("Auction", Auction);
