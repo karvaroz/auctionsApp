8afc09386239922c55ae97ddf7eb3cc4ceda7fa4
karvaroz <vargasriosk@gmail.com>
2023-06-05 22:37:57 -0500

    Created ad service

---
+++ Backend/src/services/auction.services.js
@@ -0,0 +1,25 @@
+const { AdModel, AuctionModel } = require("../models")
+
+const createNewAd = async (ad) => {
+    const { currentPrice, basePrice, timer, duration } = ad
+
+    const createAd = await new AdModel({
+        ...ad,
+        currentPrice: basePrice,
+        timer: duration
+    })
+
+    const auction = await new AuctionModel({
+        ad: createAd._id
+    })
+
+    await auction.save()
+
+    createAd.auction = auction._id
+
+    return await createAd.save();
+}
+
+module.exports = {
+    createNewAd
+}
