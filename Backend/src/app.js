5f1405992014dd278c90e1dddfd94a2f3ac556e5
karvaroz <vargasriosk@gmail.com>
2023-06-09 18:52:12 -0500

    fixed app js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,29 +1,53 @@
 require("dotenv").config();
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
+const express = require('express');
+const http = require('http');
 const cors = require("cors");
 const morgan = require("morgan");
 
 const { MongoDB } = require("./database");
-const { ErrorHandler, NotFoundHandler } = require("./middlewares");
+const { ErrorHandler, NotFoundHandler, Socket } = require("./middlewares");
 const { AuthRouter, AuctionRouter } = require("./routes");
+const { Server } = require("socket.io");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
-const server = http.createServer(app);
+const httpServer = http.createServer(app);
+const io = new Server(httpServer)
 
-const io = new Server(server, { cors: { origin: "*" } });
-// io.on("connection", require("./utils/io"));
+io.on('connection', (socket) => {
+	console.log('### Socket IO client connected');
+	socket.on('disconnect', (reason) => {
+		console.log('### Socket IO client disconnected');
+	});
+	socket.on('leaveHome', () => {
+		socket.disconnect();
+	});
+});
+adIo.on('connect', (socket) => {
+	socket.join('testroom')
+	socket.on('joinAd', ({ ad }) => {
+		socket.join(ad.toString());
+		console.log(`User joined room ${ad}`);
+	});
+	socket.on('leaveAd', ({ ad }) => {
+		socket.leave(ad.toString());
+		console.log(`Left room ${ad}`);
+	});
+	socket.on('disconnect', () => {
+		console.log('User has disconnect from ad');
+	});
+});
 
 app.use(cors());
 app.use(
 	morgan(":method :url :status :res[content-length] - :response-time ms")
 );
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(ErrorHandler);
 app.use(NotFoundHandler);
+app.use(Socket);
 
 app.use("/api/v1", AuthRouter);
 app.use("/api/v1", AuctionRouter)
@@ -31,7 +55,7 @@ app.use("/api/v1", AuctionRouter)
 const start = async () => {
 	try {
 		await MongoDB.dbConnectionMongo();
-		server.listen(PORT, () => {
+		httpServer.listen(PORT, () => {
 			console.log(`🚀 App listening on ${PORT}`);
 		});
 	} catch (error) {
