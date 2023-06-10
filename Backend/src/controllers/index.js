4eb110fdc6e0271356dce7657167c83f06651f07
karvaroz <vargasriosk@gmail.com>
2023-06-09 21:47:03 -0500

    updated barrel

--- Backend/src/controllers/index.js
+++ Backend/src/controllers/index.js
@@ -1,5 +1,5 @@
 exports.AdController = require("./ad.controller")
 exports.AuctionController = require("./auction.controller")
-exports.UserController = require("./user.controller")
+exports.AuthController = require("./auth.controller")
 exports.BidController = require("./bid.controller")
-
+exports.RoomController = require("./room.controller")
