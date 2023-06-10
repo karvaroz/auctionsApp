0074c3b15bbdf6e14db491ced2f69df90667fd19
karvaroz <vargasriosk@gmail.com>
2023-06-09 21:46:52 -0500

    created bid controller

--- Backend/src/controllers/bid.controller.js
+++ Backend/src/controllers/bid.controller.js
@@ -1,39 +1,44 @@
+const { AdModel } = require("../models");
+
 const addBid = async (req, res) => {
     const { adId } = req.params;
     const { amount } = req.query;
+
+    const ad = await AdModel.findById(adId);
+
+    if (!ad) {
+        return res
+            .status(404)
+            .json({ status: "FAILED", data: { error: "Not Found" } });
+    }
+
+    if (parseFloat(amount) <= parseFloat(ad.currentPrice)) {
+        return res.status(400).json({
+            status: "FAILED",
+            data: { error: "Your bid cannot be less than the current price" },
+        });
+    }
+
+    if (ad.status == "Upcoming" || ad.status == "Closed") {
+        return res.status(400).json({
+            status: "FAILED",
+            data: {
+                error:
+                    "As the auction has not yet begun or ended, you are unable to put a bid",
+            },
+        });
+    }
+
     try {
-        const ad = await AdModel.findById(adId);
+        const bids = ad.bids.push({ user: req.user, amount: amount })
 
-        if (!ad) {
-            return res
-                .status(404)
-                .json({ status: "NOT_FOUND", data: { error: "Not Found" } });
-        }
-
-        if (parseFloat(amount) <= parseFloat(ad.currentPrice)) {
-            return res.status(400).json({
-                status: "FAILED",
-                data: { error: "Your bid cannot be less than the current price" },
-            });
-        }
-
-        if (ad.status == "Upcoming" || ad.status == "Closed") {
-            return res.status(400).json({
-                status: "FAILED",
-                data: {
-                    error:
-                        "As the auction has not yet begun or ended, you are unable to put a bid",
-                },
-            });
-        }
-
-        const updateAd = {
+        const updateAd = new AdModel({
             ...ad,
-            bids: ad?.bids?.push({ user: req.user, amount: amount }),
+            bids,
             currentPrice: amount,
             currentBidder: req.user
         }
-
+        )
         const adSaved = await updateAd.save();
 
         if (adSaved)
@@ -54,16 +59,16 @@ const getAllBids = async (req, res) => {
     try {
         const ad = await AdModel.find({ _id: adId });
 
-        if (!ad) {
-            res
-                .status(404)
-                .json({ status: "NOT_FOUND", data: { error: "Not Found" } });
+        if (ad) {
+            return res.status(200).json({
+                status: "OK",
+                data: ad?.bids?.length > 0 ? ad?.bids : [],
+            });
         }
+        return res
+            .status(404)
+            .json({ status: "FAILED", data: { error: "Not Found" } });
 
-        res.status(200).json({
-            status: "OK",
-            data: ad.bids,
-        });
     } catch (error) {
         res
             .status(error?.status || 500)
