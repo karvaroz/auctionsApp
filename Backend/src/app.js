2476878855bc10b0ca86fa9c56f18e14e8a946cf
karvaroz <vargasriosk@gmail.com>
2023-06-09 21:45:50 -0500

    working on sockets

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,37 +6,37 @@ const morgan = require("morgan");
 
 const { MongoDB } = require("./database");
 const { ErrorHandler, NotFoundHandler, Socket } = require("./middlewares");
-const { AuthRouter, AuctionRouter } = require("./routes");
-const { Server } = require("socket.io");
+const { AuthRouter, AuctionRouter, AdRouter, BidRouter, RoomRouter } = require("./routes");
+// const { Server } = require("socket.io");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 const httpServer = http.createServer(app);
-const io = new Server(httpServer)
+// const io = new Server(httpServer)
 
-io.on('connection', (socket) => {
-	console.log('### Socket IO client connected');
-	socket.on('disconnect', (reason) => {
-		console.log('### Socket IO client disconnected');
-	});
-	socket.on('leaveHome', () => {
-		socket.disconnect();
-	});
-});
-adIo.on('connect', (socket) => {
-	socket.join('testroom')
-	socket.on('joinAd', ({ ad }) => {
-		socket.join(ad.toString());
-		console.log(`User joined room ${ad}`);
-	});
-	socket.on('leaveAd', ({ ad }) => {
-		socket.leave(ad.toString());
-		console.log(`Left room ${ad}`);
-	});
-	socket.on('disconnect', () => {
-		console.log('User has disconnect from ad');
-	});
-});
+// io.on('connection', (socket) => {
+// 	console.log('### Socket IO client connected');
+// 	socket.on('disconnect', (reason) => {
+// 		console.log('### Socket IO client disconnected');
+// 	});
+// 	socket.on('leaveHome', () => {
+// 		socket.disconnect();
+// 	});
+// });
+// adIo.on('connect', (socket) => {
+// 	socket.join('testroom')
+// 	socket.on('joinAd', ({ ad }) => {
+// 		socket.join(ad.toString());
+// 		console.log(`User joined room ${ad}`);
+// 	});
+// 	socket.on('leaveAd', ({ ad }) => {
+// 		socket.leave(ad.toString());
+// 		console.log(`Left room ${ad}`);
+// 	});
+// 	socket.on('disconnect', () => {
+// 		console.log('User has disconnect from ad');
+// 	});
+// });
 
 app.use(cors());
 app.use(
@@ -47,10 +47,13 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(ErrorHandler);
 app.use(NotFoundHandler);
-app.use(Socket);
+// app.use(Socket);
 
-app.use("/api/v1", AuthRouter);
-app.use("/api/v1", AuctionRouter)
+app.use("/api/v1/ad", AdRouter)
+app.use("/api/v1/auction", AuctionRouter)
+app.use("/api/v1", AuthRouter) //Ok
+app.use("/api/v1/bid", BidRouter)
+app.use("/api/v1/room", RoomRouter)
 
 const start = async () => {
 	try {
