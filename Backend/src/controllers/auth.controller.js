ec95a96a681c39c239883e822d631e6cc26848c2
karvaroz <vargasriosk@gmail.com>
2023-06-09 21:46:38 -0500

    updated auth controller

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,6 +1,6 @@
-const { HandleBcrypt, GenerateToken } = require('../helpers');
-const { UserModel } = require('../models');
-const { UserService } = require('../services');
+const { HandleBcrypt, GenerateToken } = require("../helpers");
+const { UserModel } = require("../models");
+const { UserService } = require("../services");
 
 const loginUser = async (req, res) => {
 	try {
@@ -10,29 +10,32 @@ const loginUser = async (req, res) => {
 
 		if (!user) {
 			return res.status(404).json({
-				status: 'FAILED',
-				data: { error: 'Email not found, please register' },
+				status: "FAILED",
+				data: { error: "Email not found, please register" },
 			});
 		}
 
-		const isPasswordCorrect = await HandleBcrypt.compare(password, user.password);
+		const isPasswordCorrect = await HandleBcrypt.compare(
+			password,
+			user.password
+		);
 
 		if (!isPasswordCorrect) {
 			return res.status(401).json({
-				status: 'FAILED',
-				data: { error: 'Incorrect password' },
+				status: "FAILED",
+				data: { error: "Incorrect password" },
 			});
 		}
 
 		const token = await GenerateToken.tokenSign(user);
 
 		res.status(200).json({
-			status: 'OK',
+			status: "OK",
 			token,
 		});
 	} catch (error) {
 		res.status(error.status || 500).json({
-			status: 'FAILED',
+			status: "FAILED",
 			data: { error: error.message || error },
 		});
 	}
@@ -43,11 +46,11 @@ const registerUser = async (req, res) => {
 		const user = req.body;
 		const createUser = await UserService.createNewUser(user);
 		if (createUser) {
-			res.status(201).json({ status: 'OK', data: createUser });
+			res.status(201).json({ status: "OK", data: createUser });
 		}
 	} catch (error) {
 		res.status(error.status || 500).json({
-			status: 'FAILED',
+			status: "FAILED",
 			data: { error: error.message || error },
 		});
 	}
