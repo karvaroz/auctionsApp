6ebe888e3d6c0b7367b359ab05e29eb0b8613077
karvaroz <vargasriosk@gmail.com>
2023-06-11 14:27:15 -0500

    update barrel file

--- Backend/src/middlewares/index.js
+++ Backend/src/middlewares/index.js
@@ -1,15 +1,17 @@
-exports.ErrorHandler = require("./handlers/errorHandler")
-exports.NotFoundHandler = require("./handlers/notFoundHandler")
+exports.ErrorHandler = require("./handlers/errorHandler");
+exports.NotFoundHandler = require("./handlers/notFoundHandler");
 
-exports.LoginSchema = require("./joiSchemas/login.schema")
-exports.UserSchema = require("./joiSchemas/user.schema")
-exports.AdSchema = require("./joiSchemas/ad.schema")
+exports.LoginSchema = require("./joiSchemas/login.schema");
+exports.UserSchema = require("./joiSchemas/user.schema");
+exports.AdSchema = require("./joiSchemas/ad.schema");
 
-exports.LoginSchemaValidation = require("./validations/login.middleware")
-exports.UserSchemaValidation = require("./validations/user.middleware")
-exports.AdSchemaValidation = require("./validations/ad.middleware")
+exports.LoginSchemaValidation = require("./validations/login.middleware");
+exports.UserSchemaValidation = require("./validations/user.middleware");
+exports.AdSchemaValidation = require("./validations/ad.middleware");
 
-exports.isAuthenticated = require("./validations/isAuth.middleware")
-exports.isRoleAdmin = require("./validations/isRoleAdmin.middleware")
+exports.isAuthenticated = require("./validations/isAuth.middleware");
+exports.isRoleAdmin = require("./validations/isRoleAdmin.middleware");
 
-exports.Socket = require("./validations/socket.middleware")
+exports.ValidMongoId = require("./validations/validMongoId");
+
+exports.Socket = require("./validations/socket.middleware");
