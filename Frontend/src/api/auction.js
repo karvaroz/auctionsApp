d25d4b79ba93164fdc8eff79fcdcd6111b9b85f2
karvaroz <vargasriosk@gmail.com>
2023-06-11 14:19:45 -0500

    auction api

--- Frontend/src/api/auction.js
+++ Frontend/src/api/auction.js
@@ -1,28 +1,28 @@
-import axios from "axios";
-import { API_URL } from "../config";
+import axios from "axios"
+import { API_URL } from "./axios"
 
-const token = JSON.parse(localStorage.getItem("token"));
+const token = JSON.parse(localStorage.getItem("token"))
 
 export const getAllAdsRequest = async () => {
-    return await axios.get(`${API_URL}/ad`, {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    });
-};
+  return await axios.get(`${API_URL}/ad`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+}
 
 export const createAdRequest = async (ad) => {
-    return await axios.post(`${API_URL}/ad`, ad, {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    });
-};
+  return await axios.post(`${API_URL}/ad`, ad, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+}
 
 export const getAdByIdRequest = async (adId) => {
-    return await axios.get(`${API_URL}/ad/${adId}`, {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    });
+  return await axios.get(`http://localhost:3000/api/v1/ad/${adId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
 }
