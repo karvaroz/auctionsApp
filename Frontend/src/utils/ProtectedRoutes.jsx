ebab08fc0bfc9b821db6473351b905f44f673dc4
karvaroz <vargasriosk@gmail.com>
2023-06-11 14:22:32 -0500

    protected route frontend

--- Frontend/src/utils/ProtectedRoutes.jsx
+++ Frontend/src/utils/ProtectedRoutes.jsx
@@ -1,13 +1,12 @@
-import { Navigate, Outlet } from "react-router-dom";
-import { useGlobalState } from "../context/AuthContext";
+import { Navigate, Outlet } from "react-router-dom"
+import { useGlobalState } from "../context/AuthContext"
 
 const ProtectedRoutes = () => {
-  const { isAuthenticated, user } = useGlobalState();
+  const { isAuthenticated, user } = useGlobalState()
 
   if (!isAuthenticated) return <Navigate to="/" replace />
 
   return <Outlet />
-
 }
 
 export default ProtectedRoutes
