b17578048a5e6548d3698eb82ac77edb8e4dbbbd
karvaroz <vargasriosk@gmail.com>
2023-06-11 14:23:11 -0500

    connectiong socket frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,66 +1,48 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import io from "socket.io-client"
 
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
-import ListPage from "./pages/ListPage";
-import CreatePage from "./pages/CreatePage";
-import DetailPage from "./pages/DetailPage";
-import NotFoundPage from "./pages/NotFoundPage";
-import Header from "./components/Header";
+import LoginPage from "./pages/LoginPage"
+import RegisterPage from "./pages/RegisterPage"
+import ListPage from "./pages/ListPage"
+import CreatePage from "./pages/CreatePage"
+import DetailPage from "./pages/DetailPage"
+import NotFoundPage from "./pages/NotFoundPage"
+import Header from "./components/Header"
 
-import ProtectedRoutes from "./utils/ProtectedRoutes";
-import { useGlobalState } from "./context/AuthContext";
+import ProtectedRoutes from "./utils/ProtectedRoutes"
+import { useGlobalState } from "./context/AuthContext"
+import { useEffect } from "react"
 
 const App = () => {
-  const { isAuthenticated, user } = useGlobalState();
+  const { isAuthenticated, user } = useGlobalState()
+  const socket = io("http://localhost:3000")
+
+  useEffect(() => {
+    socket.on("userLoggedIn", (token) => {
+      console.log("User logged in", token)
+    })
+  }, [])
+
   return (
     <BrowserRouter>
-
       {isAuthenticated && <Header />}
 
       <Routes>
-        <Route
-          exact
-          path="/"
-          element={<LoginPage />}
-        />
-        <Route
-          exact
-          path="/register"
-          element={<RegisterPage />}
-          replace
-        />
+        <Route exact path="/" element={<LoginPage />} />
+        <Route exact path="/register" element={<RegisterPage />} replace />
 
         <Route element={<ProtectedRoutes />}>
-          <Route
-            exact
-            path="/home"
-            element={<ListPage />}
-            replace
-          />
-          {user?.role == "Admin" &&
-            <Route
-              exact
-              path="/create"
-              element={<CreatePage />}
-              replace
-            />}
-          <Route
-            exact
-            path="/detail/:adId"
-            element={<DetailPage />}
-            replace
-          />
-
+          <Route exact path="/home" element={<ListPage />} replace />
+          {user?.role == "Admin" && (
+            <Route exact path="/create" element={<CreatePage />} replace />
+          )}
+          <Route exact path="/detail/:adId" element={<DetailPage />} replace />
         </Route>
 
-        <Route
-          path="*"
-          element={<NotFoundPage />}
-        />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
-  );
-};
+  )
+}
 
-export default App;
+export default App
