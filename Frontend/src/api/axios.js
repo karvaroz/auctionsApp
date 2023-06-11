cc6ac1012cd9035b8f1a20998b67902b8feb470a
karvaroz <vargasriosk@gmail.com>
2023-06-11 14:20:11 -0500

    config axios

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,10 +1,11 @@
-import axios from "axios";
-import { API_URL } from "../config";
+import axios from "axios"
+
+export const API_URL =
+  import.meta.env.VITE_API_URL || "http://localhost:3000/api/v1"
 
 const instance = axios.create({
-    baseURL: API_URL,
-    timeout: 5000,
-    // withCredentials: true,
-});
+  baseURL: API_URL,
+  timeout: 5000,
+})
 
-export default instance;
+export default instance
