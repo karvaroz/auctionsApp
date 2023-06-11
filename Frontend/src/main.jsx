1b1798cfbd136cd5531b8357a0c3d05886b17227
karvaroz <vargasriosk@gmail.com>
2023-06-11 14:23:42 -0500

    main contexts frontend

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react"
+import ReactDOM from "react-dom/client"
 
-import App from './App.jsx'
-import './index.css'
+import App from "./App.jsx"
+import "./index.css"
 
-import { ContextProvider } from './context/AuthContext.jsx'
-import { AuctionProvider } from './context/AuctionContext.jsx'
+import { ContextProvider } from "./context/AuthContext.jsx"
+import { AuctionProvider } from "./context/AuctionContext.jsx"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ContextProvider>
       <AuctionProvider>
         <App />
       </AuctionProvider>
     </ContextProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
